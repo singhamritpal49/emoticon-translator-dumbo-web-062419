0b1016f7b9086aa8746b294bce02e698dc51e1c6
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-18 16:05:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,12 @@ def load_library
   emoticon_lib = {'get_meaning'  => {},
                   'get_emoticon' => {} }
 emoticons.each do |meaning, value|
-binding.pry
-end
+  english = value[0]
+    japanese = value[1]
+    emoticon_lib['get_meaning'][japanese] = meaning
+    emoticon_lib['get_emoticon'][english] = japanese
+  end
+  emoticon_lib
 end
 
 def get_japanese_emoticon
